94b91c898e90bf022402c74f15b0d32c6bf513a0
MarioMoreira123 <a13827@agrcanelas.com>
2025-12-09 15:41:36 +0000

    9-12-25 3
    fazer mudanças ao header

--- header.php
+++ header.php
@@ -1,14 +1,10 @@
 <header>
-    <div class="nav">
-    <header>
     <div class="nav">  
     <ul>
         <li class="home"><a href="index.php">Home</a></li>
         <li class="about"><a href="about.php">sobre</a></li>
-        <li class="donate"><a href="news.php">doar</a></li>
+        <li class="donate"><a href="donate.php">doar</a></li>
         <li class="login"><a href="login.php">login</a></li>  
     </ul>     
-    </div>
-    </header>
     </div>
   </header>
--- login.php
+++ login.php
@@ -12,7 +12,7 @@
     <ul>
         <li class="home"><a href="index.php">Home</a></li>
         <li class="about"><a href="about.php">sobre</a></li>
-        <li class="donate"><a href="news.php">doar</a></li>
+        <li class="donate"><a href="donate.php">doar</a></li>
         <li class="login"><a href="login.php">login</a></li>  
     </ul>     
     </div>
