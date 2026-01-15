9cd32ee3130f4de4080c04c3803dd2275dec4a81
MarioMoreira123 <a13827@agrcanelas.com>
2026-01-15 11:46:14 +0000

    15-1-26

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
     <div class="nav">  
     <ul>
         <li class="home"><a href="index.php">Home</a></li>
-        <li class="about"><a href="about.php">sobre</a></li>
-        <li class="donate"><a href="donate.php">doar</a></li>
-        <li class="login"><a href="login.php">login</a></li>  
+        <li class="about"><a href="about.php">Sobre</a></li>
+        <li class="donate"><a href="donate.php">Doar</a></li>
+        <li class="login"><a href="login.php">Login</a></li>  
     </ul>     
     </div>
   </header>
